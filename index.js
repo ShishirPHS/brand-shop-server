6bf5252741c275a606129b85758ceb4b67af097c
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-10-19 23:29:40 +0600

    create product to database

--- index.js
+++ index.js
@@ -26,6 +26,17 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const productCollection = client.db("productDB").collection("product");
+
+    // create product
+    app.post("/product", async (req, res) => {
+      const newProduct = req.body;
+      console.log(newProduct);
+
+      const result = await productCollection.insertOne(newProduct);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
